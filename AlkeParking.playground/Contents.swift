e7667a02727a8cf51b8bc618780eae8bd420357f
edwardbecerra <edwardi.becerra@mercadolibre.cl>
2021-12-22 18:13:08 -0300

    Solutions to exercise 6 to 12
    Co-authored-by: fagdevmeli flavio.gomez@mercadolibre.com
    Co-authored-by: ignaciososaml ignacio.sosa@mercadolibre.com

--- AlkeParking.playground/Contents.swift
+++ AlkeParking.playground/Contents.swift
@@ -12,7 +12,7 @@ protocol Parkable {
 enum VehicleType {
     case car, moto, miniBus, bus
     
-    var fee: Int {
+    var hourFee: Int {
         switch self {
         case .car:
             return 20
@@ -47,6 +47,7 @@ struct Vehicle: Parkable, Hashable {
 
 struct Parking {
     private var vehicles: Set<Vehicle> = []
+    var listVehiclesCheckout: [(String, Double)] = []
     private let maxVehiclesAllow: Int = 20
     
     mutating func checkInVehicle(_ vehicle: Vehicle, onFinish: (Bool) -> Void) {
@@ -56,6 +57,72 @@ struct Parking {
         }
         onFinish(true)
     }
+    
+    mutating func checkOutVehicle (_ plate: String, onSuccess: (Double, String) -> (), onError: (String) -> ()){
+        guard let vehicle = vehicles.first(where: {$0.plate == plate}) else {
+            onError("Sorry, the check-out failed")
+            return
+        }
+        
+        var hasDiscount: Bool = false
+        let discount = vehicle.discountCard
+        if discount != nil { hasDiscount = true }
+        let fee = calcFee(type: vehicle.type, parkedTime: Double(vehicle.parkedTime), discountCard: hasDiscount)
+        
+        // SAVE ADMIN INFO
+        listVehiclesCheckout.append((plate, fee.0))
+        
+        // VEHICLE REMOVE
+        vehicles.remove(vehicle)
+        
+        onSuccess(fee.0, fee.1)
+    }
+    
+    //CALC FEE
+    
+    func calcFee(type: VehicleType, parkedTime: Double, discountCard: Bool) -> (Double, String) {
+        let initial = Double(type.hourFee)
+        var extraTime: Double?
+        var extraTimePrice: Double?
+        var discount: Double = 0
+        
+        if parkedTime > 120 {
+            extraTime = parkedTime - 120
+            extraTimePrice = (ceil(extraTime ?? 0 / 15)) * 5
+        }
+        
+        var fee = initial + (extraTimePrice ?? 0)
+        if discountCard {
+            discount = fee - fee * 0.85
+            fee = fee * 0.85
+        }
+        
+        guard let hasExtra = extraTime, let extraPrice = extraTimePrice else{
+            return (fee, "Your fee: $\(fee) details: \n Basic fee: $\(initial) \n Discount: \(discount) \n Come back soon")
+        }
+        
+        return (fee, "Your fee: $\(fee) details: \n Basic fee: $\(initial) \n Extra fee (\(hasExtra) minutes): $\(extraPrice) \n Discount: \(discount) \n Come back soon"  )
+    }
+    
+    func adminStatus(){
+        var Counter: Int = 0
+        var Profits: Double = 0
+        
+        for listVehiclesCheckout in listVehiclesCheckout {
+            Counter += 1
+            Profits += listVehiclesCheckout.1
+        }
+        
+        print("\(Counter) vehicles have checked out and have earnings of \(Profits)")
+    }
+    
+    func listVehicles(){
+        print("Parked Vehicles")
+        for vehicle in vehicles {
+            print(vehicle.plate)
+        }
+    }
+    
 }
 
 var alkeParking = Parking()
@@ -132,5 +199,13 @@ for vehicle in vehiclesTest {
     }
 }
 
+for idx in 0...10 {
+    alkeParking.checkOutVehicle(vehiclesTest[idx].plate) { fee, message in
+        print(message)
+    } onError: { Error in
+        print(Error)
+    }
+}
 
-
+alkeParking.listVehicles()
+alkeParking.adminStatus()
